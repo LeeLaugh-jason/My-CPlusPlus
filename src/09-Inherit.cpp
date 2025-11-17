93fe0e3db9d8a945a32a70e7471ce1dc81ad5fbd
leelaugh <2560536821@qq.com>
2025-11-17 19:35:03 +0800

    Inheritfinial

--- src/09-Inherit.cpp
+++ src/09-Inherit.cpp
@@ -4,6 +4,13 @@ using namespace std;
 class Animal {
 public:
     string name;
+
+private:
+    string type;
+    // void typee(){
+    //     cout << "Animal type" << endl;
+    // }
+public:
     Animal(string n){}
     void speak(){
         cout << "Animal speaks" << endl;
@@ -18,9 +25,28 @@ public:
     }
 };
 
+class Base{
+    private:
+    int b = 0;
+    public:
+    int a;
+    void output(){
+        cout << "Base output b = " << b << endl;
+    }
+    
+    protected:
+    int c;
+};
+
 int main(){
-    Dog dog("Buddy");
-    dog.speak();  // 输出: Dog speak
-    dog.barks();  // 输出: Dog barks
+    // Dog dog("Buddy");
+    // dog.speak();  // 输出: Dog speak
+    // dog.barks();  // 输出: Dog barks
+    // dog.typee();   // 输出: Animal type
+    Base obj;
+    obj.output(); // 可以访问 public 成员
+    // obj.b = 20; // 错误: 无法访问 private 成员
+    // obj.c = 30; // 错误: 无法访问 protected 成员
+
     return 0;
 }
