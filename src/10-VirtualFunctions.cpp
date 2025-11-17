21c0b23ec1c0d4732397d98e8076fa650c670343
leelaugh <2560536821@qq.com>
2025-11-17 23:12:52 +0800

    virtualfunction

--- src/10-VirtualFunctions.cpp
+++ src/10-VirtualFunctions.cpp
@@ -8,6 +8,7 @@ class Animal{
         virtual void speak(){
             std::cout << "Animal speaks" << std::endl;
         }
+        virtual void spark() = 0; // 纯虚函数
 };
 
 class Dog  : public Animal{
@@ -17,11 +18,15 @@ class Dog  : public Animal{
         void speak() override {
             std::cout << "Dog barks" << std::endl;
         }
+        void spark() override {
+            std::cout << "Dog sparks" << std::endl;
+        }
 };
 
 int main(){
     Animal* animalPtr = new Dog();
     animalPtr->speak();  // 输出: Dog barks
+    animalPtr->spark();  // 输出: Dog sparks
 
     delete animalPtr;
     return 0;
