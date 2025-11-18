89eb1dd37dd17d9dfcb96a858d5abe8a33230272
leelaugh <2560536821@qq.com>
2025-11-18 21:37:13 +0800

    ThisPtrOver

--- src/11-ThisPtr.cpp
+++ src/11-ThisPtr.cpp
@@ -16,8 +16,51 @@ class Point {
         }
 };
 
-int main(){
-    Point p(10, 20);
-    p.display(); // 输出: Point(10, 20)
+class Sample{
+    private:
+        int value;
+    public:
+        void SamplePtr(){
+            cout << "SamplePtr's address: " << this << endl;
+        }
+};
+
+class Calculate{
+    private:
+        int data;
+    public:
+        Calculate& setData(int data){
+            this->data = data;
+            return *this; // 返回当前对象的引用
+        }
+        Calculate& add(int value){
+            this->data += value;
+            return *this; // 返回当前对象的引用
+        }
+        Calculate& subtract(int value){
+            this->data -= value;
+            return *this; // 返回当前对象的引用
+        }
+        Calculate& multiply(int value){
+            this->data *= value;
+            return *this; // 返回当前对象的引用
+        }
+        Calculate& divide(int value){
+            if(value != 0){
+                this->data /= value;
+            }
+            return *this; // 返回当前对象的引用
+        }
+        void display(){
+            cout << "Result: " << this->data << endl;
+        }
+};
+
+int main_This(){
+    Calculate calc;
+    const Calculate& calcRef = calc.setData(10).add(5).subtract(3).multiply(4).divide(2);
+    calcRef.display(); // 输出: Result: 26
     return 0;
 }
+
+
